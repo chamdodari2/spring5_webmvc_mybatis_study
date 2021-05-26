7268cbe6665fc893add504d4fdeb63bf66215976
yjkim <chitose_@naver.com>
2021-05-26 12:20:19 +0900

    회원조회 화면에서 상세보기 넘어갈때 여러가지 에러 발생가능->하나의 화면으로 퉁치기

--- src/main/java/spring5_webmvc_mybatis_study/controller/MemberDetailController.java
+++ src/main/java/spring5_webmvc_mybatis_study/controller/MemberDetailController.java
@@ -1,7 +1,9 @@
 package spring5_webmvc_mybatis_study.controller;
 
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
